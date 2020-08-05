ccb84746344b3b4e93d4ca95cf65fa52695be280
José Benavente <jose@bgmp.cl>
2020-08-05 16:46:49 -0400

    Fix misleading test descriptions in gem spec

--- spec/minecraft/data_spec.rb
+++ spec/minecraft/data_spec.rb
@@ -3,11 +3,11 @@ RSpec.describe Minecraft::Data do
     expect(Minecraft::Data::VERSION).not_to be nil
   end
 
-  it 'Tests converting a Minecraft uuid to its current username' do
+  it 'Tests converting a Minecraft username to its current uuid' do
     expect(Minecraft::Data.username_to_uuid('Notch')).to eql('069a79f4-44e9-4726-a5be-fca90e38aaf5')
   end
 
-  it 'Tests converting a Minecraft username to its uuid' do
+  it 'Tests converting a Minecraft uuid to its current username' do
     expect(Minecraft::Data.uuid_to_username('069a79f4-44e9-4726-a5be-fca90e38aaf5')).to eql('Notch')
   end
 
@@ -23,7 +23,7 @@ RSpec.describe Minecraft::Data do
     expect(Minecraft::Data.head_url_of_uuid('BGMP')).to eql('https://api.ashcon.app/mojang/v2/avatar/BGMP')
   end
 
-  it 'Tests retrieving a Minecraft skin head url from a username' do
+  it 'Tests retrieving a Minecraft skin head url from a uuid' do
     expect(Minecraft::Data.head_url_of_uuid('2cef6d05-ec9a-44b3-bedc-53359c68ae65')).to eql('https://api.ashcon.app/mojang/v2/avatar/2cef6d05-ec9a-44b3-bedc-53359c68ae65')
   end
 end
